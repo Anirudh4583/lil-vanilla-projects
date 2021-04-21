789b710b08aef3c0455e01fde04a6b99ff4fc82b
anirudh4583 <201951024@iiitvadodara.ac.in>
2021-04-22 00:27:01 +0530

    scroll anim

--- 1.expanding-cards/script.js
+++ 1.expanding-cards/script.js
@@ -1,17 +1,17 @@
-const varPanels = document.querySelectorAll('.panel')
+const varPanels = document.querySelectorAll('.panel');
 
 varPanels.forEach((panel) => {
-    panel.addEventListener('click', () => {
-        console.log('clicked')
-        removeActiveClass()
-        panel.classList.add('active')
-        console.log('added active class')
-    })
-})
+  panel.addEventListener('click', () => {
+    console.log('clicked');
+    removeActiveClass();
+    panel.classList.add('active');
+    console.log('added active class');
+  });
+});
 
 function removeActiveClass() {
-    varPanels.forEach((panel) => {
-        console.log('removed all active classes')
-        panel.classList.remove('active')
-    })
+  varPanels.forEach((panel) => {
+    console.log('removed all active classes');
+    panel.classList.remove('active');
+  });
 }
