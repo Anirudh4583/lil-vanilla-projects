6cb6573370a67fe203e9e9d7e703536536e2cf75
anirudh4583 <201951024@iiitvadodara.ac.in>
2021-05-28 01:10:04 +0530

    dad jokes

---
+++ dad-jokes/script.js
@@ -0,0 +1,29 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+
+async function generateJoke() {
+  const res = await fetch('https://icanhazdadjoke.com', {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
+}
+
+// function generateJoke() {
+//     fetch('https://icanhazdadjoke.com', {
+//       headers: {
+//         Accept: 'application/json',
+//       },
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         jokeEl.innerHTML = data.joke;
+//       });
+//   }
